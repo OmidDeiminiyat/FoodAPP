07938607779e6a5fb442b9d690c57eb5f6405217
elop123 <142408289+elop123@users.noreply.github.com>
2024-03-12 11:16:11 +0100

    js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,9 @@
-// We're gonna write some cool JS in this file!
+//
+fetch('https://www.themealdb.com/api/json/v1/1/categories.php')
+.then(res => {
+    return res.json();
+})
+.then(data=>{
+    console.log(data);
+})
+
