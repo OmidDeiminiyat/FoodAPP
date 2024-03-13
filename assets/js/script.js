5048a01929880b0590b733cc7e87ca71b6d36ab1
maylova93 <142877128+maylova93@users.noreply.github.com>
2024-03-13 11:04:15 +0100

    domletter

--- assets/js/script.js
+++ assets/js/script.js
@@ -35,3 +35,42 @@ function showListMeal(recipeList) {
         recipeContainer.innerHTML += html;
     });
 }
+// JavaScript Buttons
+document.getElementById('button1').addEventListener('click', function() {
+    alert('Button1');
+  });
+  
+  document.getElementById('button2').addEventListener('click', function() {
+    alert('Button2');
+  });
+  
+  document.getElementById('button3').addEventListener('click', function() {
+    alert('button3');
+    
+  });
+  document.getElementById('button4').addEventListener('click', function() {
+    alert('Button4');
+  });
+  
+  document.getElementById('button5').addEventListener('click', function() {
+    alert('Button5');
+  });
+  
+  document.getElementById('button6').addEventListener('click', function() {
+    alert('button6');
+    
+  });
+  document.getElementById('button7').addEventListener('click', function() {
+    alert('Button7');
+  });
+  
+  document.getElementById('button8').addEventListener('click', function() {
+    alert('Button8');
+  });
+  
+  document.getElementById('button9').addEventListener('click', function() {
+    alert('button9');
+    
+  });
+  
+  
